43ae217a176e4fdcd6c318eee203f9b2f038fcbe
jonathanSoderlund <jonathan.soderlund98@gmail.com>
2024-12-10 15:06:33 +0100

    Update uppgift.java

--- uppgift.java
+++ uppgift.java
@@ -6,5 +6,35 @@ public class Dongeon {
   
 public class Room {
 
-public void doNarative(String[] args)
+public void doNarative(String[] args) {
   int[] doors = {1,2}
+
+  if (1) {
+            System.out.println("You stand in a large cave and a bat flies by. “ \n" +
+                    "It is dark and cold and you can not see very well. “\n" +
+                    "\n" +
+                    "Press 1 to eastern door. “\n" +
+                    "Press 2 to southern door. “\n");
+        }
+        else if (2) {
+            System.out.println("In this room, four torches hang from the walls. “ \n" +
+                    "The light is warm. “\n" +
+                    "\n" +
+                    "Press 1 to eastern door. “\n" +
+                    "Press 2 to northern door. “\n");
+        }
+        else if (3) {
+            System.out.println("You have come to a narrow tunnel. “ \n" +
+                    "There is water on the floor that reaches your knees. “ \n" +
+                    "\n" +
+                    "Press 1 to western door. “\n" +
+                    "Press 2 to southern door. “\n");
+        }
+        else (4) {
+            System.out.println("You come in and see chest and you see little golden light through the keyhole. “\n" +
+                    "\n" +
+                    "Press 1 to northern door. “\n" +
+                    "Press 2 to western door. “\n");
+        }
+}
+}
